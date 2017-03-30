90747bb45d5e950093722133c11455e7da5a6e1c
Lincoln Dennis <j.lincoln.dennis@gmail.com>
2017-03-30 16:31:17 -0600

    add a fest form built, not wired yet

--- app/scripts/post.directive.js
+++ app/scripts/post.directive.js
@@ -9,9 +9,31 @@
         restrict: 'E',
         scope: {},
         templateUrl: '/views/posts.html',
-        // controller: headerController,
-        // controllerAs: 'vm'
+        controller: PostController,
+        controllerAs: 'vm'
       };
+    }
+
+    PostController.$inject = ['$log'];
+
+    function PostController($log) {
+      const vm = this;
+      vm.bands = [{id: 'band1'}];
+      vm.addBand = addBand;
+      vm.removeBand = removeBand;
+
+      function addBand() {
+        var newBandNo = vm.bands.length + 1;
+        vm.bands.push({id: 'band'+newBandNo})
+      }
+
+      function removeBand() {
+        var lastBand = vm.bands.length -1;
+        vm.bands.splice(lastBand);
+
+      }
+
+
     }
 
 
