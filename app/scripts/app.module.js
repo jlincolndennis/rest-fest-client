5b28a1dd4993167c305160e2e3c2b9cc4b3fb53f
Lincoln Dennis <j.lincoln.dennis@gmail.com>
2017-03-30 18:53:53 -0600

    cleaning up files and whatnot

--- app/scripts/app.module.js
+++ app/scripts/app.module.js
@@ -8,22 +8,6 @@
   ];
 
   angular.module('clientApp', dependencies);
-  //   .config(setupRoutes)
-  //
-  // setupRoutes.$inject = ['$stateProvider',
-  //                     '$urlRouterProvider',
-  //                     '$locationProvider',
-  //                     '$httpProvider'];
-  //
-  // function setupRoutes($stateProvider, $urlRouterProvider, $locationProvider, $httpProvider){
-  //   $locationProvider.html5Mode(true);
-  //   $urlRouterProvider.otherwise("/");
-  //
-  //   $stateProvider
-  //     .state('home', {
-  //       url: '/',
-  //       template: ''
-  //     })
-  //   }
+
 
 }());
